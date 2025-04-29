bc72799703f53261fe788c9a069888d1141b5e9d
mrparihar08 <preet0810parihar@gmail.com>
2025-04-29 07:45:22 +0530

    7 commit

--- backend/vitya.py
+++ backend/vitya.py
@@ -347,4 +347,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         fix_expense_dates()
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
