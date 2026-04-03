5f8e5ad7b09bc8e5ae1f0214a21db2eeabd57f97
mrparihar08 <preet0810parihar@gmail.com>
2026-04-03 12:03:47 +0530

    10m

--- backend/chats/utils/media_and_exports.py
+++ backend/chats/utils/media_and_exports.py
@@ -1,5 +1,6 @@
 import base64
 import io
+from typing import Optional
 
 import qrcode
 import barcode
@@ -12,12 +13,13 @@ from backend.chats.utils.document_generators import (
 )
 from backend.chats.utils.presentation_generators import generate_ppt_from_text
 
+
 def generate_qr(data: str) -> str:
     qr = qrcode.make(data)
     buffer = io.BytesIO()
     qr.save(buffer, format="PNG")
-
     return base64.b64encode(buffer.getvalue()).decode("utf-8")
+
 
 def generate_barcode(data: str) -> str:
     CODE128 = barcode.get_barcode_class("code128")
@@ -26,10 +28,11 @@ def generate_barcode(data: str) -> str:
     code.write(buffer)
     return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
+
 def generate_all_files(text: str, user_title: Optional[str] = None):
-        return {
-            "csv": generate_csv_from_text(text, user_title=user_title),
-            "docx": generate_doc_from_text(text, user_title=user_title),
-            "pdf": generate_pdf_from_text(text, user_title=user_title),
-            "pptx": generate_ppt_from_text(text, user_title=user_title),
-        }
+    return {
+        "csv": generate_csv_from_text(text, user_title=user_title),
+        "docx": generate_doc_from_text(text, user_title=user_title),
+        "pdf": generate_pdf_from_text(text, user_title=user_title),
+        "pptx": generate_ppt_from_text(text, user_title=user_title),
+    }
